1a54a0e7cd1c44f3a9dda195eb53a3bf7bf2d2b5
steveroettger <steve@steveroettger.com>
2017-11-21 16:32:41 -0500

    Added steve credentials for push

--- README.md
+++ README.md
@@ -43,3 +43,6 @@ BtRXoeCppesRV_DUFZrc6zkNXX4q58Ptw3BHG5gUmqU
 
 Paul's Desktop Subscription:
 {"endpoint":"https://fcm.googleapis.com/fcm/send/fz4fCqwIgCg:APA91bH69o_hEr53CAGT6Hc2nCtACj6KpRKtqLjFZ49_FSVCjxKp5uuH9d2pNCM_ozE2I8sPfNOCTWzPvskf8CX3JWlz_cEiDOKWePmfXdMVA1XY-5_h70Ty8XcA7PvLCH0yDO-bg6Hs","expirationTime":null,"keys":{"p256dh":"BGSxyqjdkGTXOBNqNNnud19zPAi1dClkVXr8GNGBaxlCK4g0rX_405ALHElbZIcWMsiaWdlwfo0NtzEA4yY5fvY=","auth":"fnkYLDsmgOIXkDiBTL1KMg=="}}
+
+Steve's Desktop Subscription:
+{"endpoint":"https://fcm.googleapis.com/fcm/send/c_Ffu-gT6bc:APA91bEWA3h7zuv3FTBsAKw50c27E4fQJbSdyFzgCUf_s12YEpSdYuMNSm5pQG9uDzMMvEvUMnXTFploGHLAxYnk1H_DsYe8nwauJ2iXzFZMRA9OAKDCrqtvZjDdEiMoU1eQiA3TEkSn","expirationTime":null,"keys":{"p256dh":"BJjmN9iOy6fPKcseGtyHGzTmYllEAOO0Zm0l59Lj7wQYYsIU9TNxIvFhZRuRl0W7lJklCz5zM5g82yUfoBOuivs=","auth":"P8TzUIBqeLia0JmQ2gdcBw=="}}
--- push-server/web-push.js
+++ push-server/web-push.js
@@ -1,6 +1,13 @@
 var webPush = require('web-push');
 
-var pushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/fz4fCqwIgCg:APA91bH69o_hEr53CAGT6Hc2nCtACj6KpRKtqLjFZ49_FSVCjxKp5uuH9d2pNCM_ozE2I8sPfNOCTWzPvskf8CX3JWlz_cEiDOKWePmfXdMVA1XY-5_h70Ty8XcA7PvLCH0yDO-bg6Hs","expirationTime":null,"keys":{"p256dh":"BGSxyqjdkGTXOBNqNNnud19zPAi1dClkVXr8GNGBaxlCK4g0rX_405ALHElbZIcWMsiaWdlwfo0NtzEA4yY5fvY=","auth":"fnkYLDsmgOIXkDiBTL1KMg=="}};
+var paulsPushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/fz4fCqwIgCg:APA91bH69o_hEr53CAGT6Hc2nCtACj6KpRKtqLjFZ49_FSVCjxKp5uuH9d2pNCM_ozE2I8sPfNOCTWzPvskf8CX3JWlz_cEiDOKWePmfXdMVA1XY-5_h70Ty8XcA7PvLCH0yDO-bg6Hs","expirationTime":null,"keys":{"p256dh":"BGSxyqjdkGTXOBNqNNnud19zPAi1dClkVXr8GNGBaxlCK4g0rX_405ALHElbZIcWMsiaWdlwfo0NtzEA4yY5fvY=","auth":"fnkYLDsmgOIXkDiBTL1KMg=="}};
+
+var stevesPushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/c_Ffu-gT6bc:APA91bEWA3h7zuv3FTBsAKw50c27E4fQJbSdyFzgCUf_s12YEpSdYuMNSm5pQG9uDzMMvEvUMnXTFploGHLAxYnk1H_DsYe8nwauJ2iXzFZMRA9OAKDCrqtvZjDdEiMoU1eQiA3TEkSn","expirationTime":null,"keys":{"p256dh":"BJjmN9iOy6fPKcseGtyHGzTmYllEAOO0Zm0l59Lj7wQYYsIU9TNxIvFhZRuRl0W7lJklCz5zM5g82yUfoBOuivs=","auth":"P8TzUIBqeLia0JmQ2gdcBw=="}};
+
+var subscriptionKeys = {
+	paul: paulsPushSubscription,
+	steve: stevesPushSubscription
+};
 
 var vapidPublicKey = 'BB28Y98Mn9felmviJn4pKZn3pdWVx1XzkmzBoEiZ2kRE8Tv-YJLk9fZWtlH5h66JqN-f86G2ThlmySXiFMy6eEU';
 var vapidPrivateKey = 'BtRXoeCppesRV_DUFZrc6zkNXX4q58Ptw3BHG5gUmqU';
@@ -16,8 +23,12 @@ var options = {
   TTL: 60
 };
 
-webPush.sendNotification(
-  pushSubscription,
-  payload,
-  options
-);
+process.argv.forEach(function (val, index, array) {
+  if (index > 1) {
+  	webPush.sendNotification(
+		subscriptionKeys[val],
+		payload,
+		options
+	);
+  }
+});
