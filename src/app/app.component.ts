1cb0453fc6141ee43d8253ab4669896a26f3e5c7
steveroettger <steve@steveroettger.com>
2017-11-21 15:23:08 -0500

    Added online footer status bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Progressive Web App (PWA) Proof-of-Concept';
+
+  public isOnline: boolean;
+
+  ngOnInit() {
+    if (navigator.onLine) {
+      this.isOnline = true;
+    } else {
+      this.isOnline = false;
+    }
+  }
+
+  @HostListener('window:offline', ['$event'])
+  offline(event) {
+    this.isOnline = false;
+  }
+
+  @HostListener('window:online', ['$event'])
+  online(event) {
+    this.isOnline = true;
+  }
 }
