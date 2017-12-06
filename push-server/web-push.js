8842f60e2bf4ffff9ec21679497cc159841670bc
steveroettger <steve@steveroettger.com>
2017-12-06 10:39:13 -0500

    Added SamsungS8 web push credentials

--- push-server/web-push.js
+++ push-server/web-push.js
@@ -4,12 +4,15 @@ var paulsPushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/dmK
 
 var stevesPushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/c_Ffu-gT6bc:APA91bEWA3h7zuv3FTBsAKw50c27E4fQJbSdyFzgCUf_s12YEpSdYuMNSm5pQG9uDzMMvEvUMnXTFploGHLAxYnk1H_DsYe8nwauJ2iXzFZMRA9OAKDCrqtvZjDdEiMoU1eQiA3TEkSn","expirationTime":null,"keys":{"p256dh":"BJjmN9iOy6fPKcseGtyHGzTmYllEAOO0Zm0l59Lj7wQYYsIU9TNxIvFhZRuRl0W7lJklCz5zM5g82yUfoBOuivs=","auth":"P8TzUIBqeLia0JmQ2gdcBw=="}};
 
-var steveGithubProd = {"endpoint":"https://fcm.googleapis.com/fcm/send/dVQtY2AnB2A:APA91bENIAddIAy07TC8kZXZpP7pPjN1wf5jZDNpkyTPHl8g47eqILmAewVB-ZQVPPcBs3nZYtoy_lEulmR2oY9gBlDsJiJKujIc615ltzd27TLhzbUowfSFJ-x6binRd6-OnDf0fdWL","expirationTime":null,"keys":{"p256dh":"BOHHJ3P7intNFG5rJfx8XeVaOmp63txuxSU2yYiDTQkedeEVBnsQwA3EBX5PEjD9KAsfB-GW6YY1rh08wNxrXOc=","auth":"G5NE9YACKdc_HmR_iBeftg=="}}
+var steveGithubProd = {"endpoint":"https://fcm.googleapis.com/fcm/send/dyDD6tP7Pq0:APA91bGrUUM2r_V_ZXMRq2mVbYGb6Ik5j-KV1imsQI2ZDWILHH0z3pCZIeztZ_iVOAGr-Oz6L-W-5JYNs_uoDAwLMWpfy0jz15rWNQCFpiFSt2NmGOVYW3HbeB5QDABPlil3TuStoWd6","expirationTime":null,"keys":{"p256dh":"BOAtQFHjFENYM3VIWKvq4V67gwXEZf_a4HhkW3MssPmTM5UHK-7u5uPTlgrULpUt4UkPtH79j8vFm6bHTpTpdaM=","auth":"uh2ChikqbSjk3WI4VbRnrA=="}};
+
+var samsungS8Subscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/e9Of3XEvSCA:APA91bEzBCL1WNenAkAU6EhXP1l1PoRKdPHxKcykQqeDh3gLZCaAmDpoi43iTCI-qRlwNsxX7EvQA6cifoTFJDbQYhi4kh6vjna1atniSfQW_Dfq2OrwwIx7Y-FfYkpmGRGLzE1d8kUH","expirationTime":null,"keys":{"p256dh":"BK8TMbOVZmowsCXLO0UUAD_oafbeDqdlklq19qRwkiMNCHkEQ4qo-OvMb5_VZ58pD-XtwWo3tEg5UAblne8r_GM=","auth":"lJFgmujbmfnS79tPaez7mQ=="}};
 
 var subscriptionKeys = {
 	paul: paulsPushSubscription,
 	steve: stevesPushSubscription,
-  steveProd: steveGithubProd
+  steveProd: steveGithubProd,
+  s8: samsungS8Subscription
 };
 
 var vapidPublicKey = 'BB28Y98Mn9felmviJn4pKZn3pdWVx1XzkmzBoEiZ2kRE8Tv-YJLk9fZWtlH5h66JqN-f86G2ThlmySXiFMy6eEU';
